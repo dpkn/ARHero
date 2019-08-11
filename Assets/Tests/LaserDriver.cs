e410f2e3f9b773679ca808db733c6a791b47894b
daniel <9087597+daankrs@users.noreply.github.com>
2019-08-11 18:58:09 +0200

    Initial commit

---
+++ Assets/Tests/LaserDriver.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserDriver : MonoBehaviour
+{
+    /*  public LineRenderer laserLineRenderer;
+      public float laserWidth = 0.1f;
+      public float laserMaxLength = 5f;
+
+      void Start()
+      {
+          Vector3[] initLaserPositions = new Vector3[2] { Vector3.zero, Vector3.zero };
+          laserLineRenderer.SetPositions(initLaserPositions);
+          laserLineRenderer.startWidth = laserWidth;
+          laserLineRenderer.endWidth = laserWidth;
+      }
+
+      void Update()
+      {
+
+              ShootLaserFromTargetPosition(transform.position, Vector3.forward, laserMaxLength);
+              laserLineRenderer.enabled = true;
+
+      }
+
+      void ShootLaserFromTargetPosition(Vector3 targetPosition, Vector3 direction, float length)
+      {
+          Ray ray = new Ray(targetPosition, direction);
+          RaycastHit raycastHit;
+          Vector3 endPosition = targetPosition + (length * direction);
+
+          if (Physics.Raycast(ray, out raycastHit, length))
+          {
+              endPosition = raycastHit.point;
+          }
+
+          laserLineRenderer.SetPosition(0, targetPosition);
+          laserLineRenderer.SetPosition(1, endPosition);
+      }*/
+    private Camera mainCamera;
+    public float range = 100f;
+    BallBehaviour previousTarget;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        Shoot();
+    }
+
+    void Shoot()
+    {
+        if (previousTarget != null)
+        {
+            previousTarget.SetFocus(false);
+            previousTarget = null;
+        }
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hit, range))
+        {
+         
+            BallBehaviour target = hit.transform.GetComponent<BallBehaviour>();
+
+
+            if (target != null)
+            {
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    target.Hit();
+                }
+                else
+                {
+                    target.SetFocus(true);
+                }
+                previousTarget = target;
+            }
+
+        }
+
+    }
+
+}
---
+++ Assets/Tests/TempoManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TempoManager : MonoBehaviour
+{
+    public float BPM = 125;
+    private float _lastSyncTime = 0;
+    private uint _beatsSinceSync = 0;
+    public float startDelay = 0;
+    public AudioSource audio1;
+    public AudioSource audio2;
+    public AudioSource audio3;
+    public AudioSource audio4;
+    public AudioSource audio5;
+    public AudioSource audio6;
+    public AudioSource audio7;
+
+    public Spawner spawner;
+  //  public Text text;
+
+    void Start()
+    {
+        SyncBPM();
+        double initTime = AudioSettings.dspTime;
+        audio1.PlayScheduled(initTime + startDelay);
+        audio2.PlayScheduled(initTime + startDelay);
+        audio3.PlayScheduled(initTime + startDelay);
+        audio4.PlayScheduled(initTime + startDelay);
+        audio5.PlayScheduled(initTime + startDelay);
+        audio6.PlayScheduled(initTime + startDelay);
+        audio7.PlayScheduled(initTime + startDelay);
+    }
+    void Update()
+    {
+        //Check beat timer and trigger beat if neccessary
+        if (Time.time > _lastSyncTime + (BeatsPerMinuteToDelay(BPM) * _beatsSinceSync))
+        {
+            Beat();
+        }
+
+       // text.text = _beatsSinceSync.ToString("0");
+    }
+    private static float BeatsPerMinuteToDelay(float beatsPerMinute)
+    {
+        //beats per second = beatsPerMinute / 60
+        return 1.0f / (beatsPerMinute / 60.0f);
+    }
+    private void Beat()
+    {
+        _beatsSinceSync++;
+
+        if (_beatsSinceSync % 4 == 0)
+        {
+            spawner.Spawn();
+        }
+
+    }
+    /// <summary>
+    /// Restart BPM timer
+    /// </summary>
+    public void SyncBPM()
+    {
+        _lastSyncTime = Time.time;
+        _beatsSinceSync = 0;
+        Beat(); //NB: beat is now synced immedately instead of after a 1 beat delay
+    }
+}
